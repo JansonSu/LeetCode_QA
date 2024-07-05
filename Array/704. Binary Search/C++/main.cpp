6b9e5f01a937ef74b90cc2a50958166ff364a29c
JansonSu <137122140+JansonSu@users.noreply.github.com>
2024-07-04 23:59:29 -0700

    Update main.cpp

--- Array/704. Binary Search/C++/main.cpp
+++ Array/704. Binary Search/C++/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,32 @@ public:
         for(int i = 0; i< nums.size(); i++){
             if(nums[i] == target){
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    int search_logO_1(std::vector<int>& nums, int target) {
+        
+        std::sort(nums.begin(), nums.end());
+
+        auto it = std::lower_bound(nums.begin(), nums.end(), target);
+        if (it != nums.end() && *it == target) {
+            return std::distance(nums.begin(), it);
+        }
+        return -1;
+    }
+
+    int search_logO_2(std::vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
