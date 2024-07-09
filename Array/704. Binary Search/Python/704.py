25b3c6e3b92be276ab44438f54e15f777b135fc3
JansonSu <137122140+JansonSu@users.noreply.github.com>
2024-07-09 00:34:17 -0700

    Update 704.py

--- Array/704. Binary Search/Python/704.py
+++ Array/704. Binary Search/Python/704.py
@@ -3,9 +3,16 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target ==nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid +1
         return -1
 
 
